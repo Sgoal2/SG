0ff93b4747653a05c065c536899c893e8fc3d495
Sag <sag@Apples-MBP.attlocal.net>
2019-03-13 13:42:52 -0400

    update user profiles

--- site-config.js
+++ site-config.js
@@ -1,7 +1,7 @@
 //const path = require('path');
 
 module.exports = {
-    name:"Mahipat",
+    name:"Sagie",
     siteTitle: "Sagie Gelbhauer, Full stack developer", // Site title.
     siteLogo: `${__dirname}/src/static/images/mahipat.jpg`,
     siteUrl: "https://www.sagieg.com", // Domain of your website without pathPrefix.
@@ -9,15 +9,15 @@ module.exports = {
     siteDescription: "I'm Sagie Gelbhauer, Full stack developer, I'm working with clients to build reliable, fast, engaging & responsive websites using Javascript, ReactJS, Redux, NodeJs, GraphQl, Bootstrap, Gatsby", // Website description used for RSS feeds/meta description tag.
     googleAnalyticsID: "UA-115817837-3", // GA tracking ID.
     userName: "Mahipat Jadav", // Username to display in the author segment.
-    userTwitter: "mhjadav", // Optionally renders "Follow Me" in the UserInfo segment.
-    userLocation: "Ahmedabad, Gujarat, India", // User location to display in the author segment.
+    userTwitter: "sagg227", // Optionally renders "Follow Me" in the UserInfo segment.
+    userLocation: "USA", // User location to display in the author segment.
     userAvatar: "../static/images/mahipat.jpg", // User avatar to display in the author segment.
     favicon32:`../static/images/mahipat32.jpg`,
     favicon16:`../static/images/mahipat16.jpg`,
     ogImage: `../static/images/mahipat250.jpg`,
     userDescription: "I'm Sagie Gelbhauer, Full stack developer, I'm working with clients from India and USA to build reliable, fast, engaging & responsive websites using Javascript, ReactJS, Redux, NodeJs, GraphQl, Bootstrap, Gatsby, Angular, Typescript",
-    userPhone:"+91-9898226919",
-    userEmail:"mj@mojaave.com",
+    userPhone:"732-551-7509",
+    userEmail:"sgelb@gmail.com",
     skills:[
         {
             name:"Javascript",
@@ -42,16 +42,12 @@ module.exports = {
     ],
     langauges:[
         {
-            name:"Gujarati",
-            rating: 10
-        },
-        {
-            name: "Hindi",
-            rating: 10
+            name: "Hebrew",
+            rating: 9
         },
         {
             name: "English",
-            rating: 8
+            rating: 10
         }
     ],
     experience:[
@@ -70,30 +66,15 @@ module.exports = {
             startDate: "March 2017",
             endDate: "March 2017",
             overview:"Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste."
-        },
-        {
-            position:"Software Enginer",
-            company: "Company 3 Pvt. Ltd.",
-            current:false,
-            startDate: "March 2017",
-            endDate: "March 2017",
-            overview:"Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste."
-        },
-        {
-            position:"Software Enginer",
-            company: "Company 4 Pvt. Ltd.",
-            current:false,
-            startDate: "March 2017",
-            endDate: "March 2017",
-            overview:"Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste."
         }
+        
     ],
     education:[
         {
-            college:"Some College of Engineering 1",
+            college:"Lambda School",
             degree:"Compouter Science And Engineering",
-            startDate:"July 2007",
-            endDate: "Jun 2011",
+            startDate:"May 2018",
+            endDate: "December 2018",
             current: true,
             overview: "Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste."
         },
@@ -104,56 +85,34 @@ module.exports = {
             endDate: "Jun 2011",
             current: false,
             overview: "Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste."
-        },
-        {
-            college:"Some College of Engineering 3",
-            degree:"Compouter Science And Engineering",
-            startDate:"July 2007",
-            endDate: "Jun 2011",
-            current: false,
-            overview: "Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste."
-        },
-        {
-            college:"Some College of Engineering 4",
-            degree:"Compouter Science And Engineering",
-            startDate:"July 2007",
-            endDate: "Jun 2011",
-            current: false,
-            overview: "Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste."
-        },
-        {
-            college:"Some College of Engineering 5",
-            degree:"Compouter Science And Engineering",
-            startDate:"July 2007",
-            endDate: "Jun 2011",
-            current: false,
-            overview: "Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste."
         }
+        
+        
     ],
     // Links to social profiles/projects you want to display in the author segment/navigation bar.
     userLinks: [
       {
         label: "GitHub",
-        url: "https://github.com/mhjadav/mojaave",
+        url: "https://github.com/Sgoal2",
         iconClassName: "fa fa-github"
       },
       {
         label: "Twitter",
-        url: "https://twitter.com/mhjadav",
+        url: "https://twitter.com/sagg227",
         iconClassName: "fa fa-twitter"
       },
       {
         label: "Email",
-        url: "mailto:mj@mojaave.com",
+        url: "mailto:sgelb2@gmail.com",
         iconClassName: "fa fa-envelope"
       },
       {
         label: "LinkedIn",
-        url: "https://www.linkedin.com/in/mahipat-jadav-987986158/",
+        url: "https://www.linkedin.com/in/sagie-g/",
         iconClassName: "fa fa-linkedin"
       }
     ],
-    copyright: "Copyright © 2018. @mahipat", // Copyright string for the footer of the website and RSS feed.
+    copyright: "Copyright © 2018. @sagie", // Copyright string for the footer of the website and RSS feed.
     themeColor: "#5a76b9", // Used for setting manifest and progress theme colors.
     backgroundColor: "#fff" // Used for setting manifest background color.
   };
